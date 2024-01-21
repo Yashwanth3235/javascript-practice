8a6a83108fed4aaea43c727e1f2ef3c3e5720d91
M Yaswanth kumar <yashwanthkumarm123@gmail.com>
2024-01-21 16:51:31 +0530

    basic Object method example

---
+++ Object_method.js
@@ -0,0 +1,10 @@
+const myobject =
+{
+    property1 : 'value1',
+    property2: 'value2',
+    mymethod : function()    
+        {
+            console.log("Hello, Grace");
+        }
+};
+myobject.mymethod();
