b28e1570dc9763e335a18bedd987b2074bf08d91
M Yashwanth kumar <135724795+Yashwanth3235@users.noreply.github.com>
2024-01-29 01:27:26 +0530

    Delete function_constructor.js

--- function_constructor.js
+++
@@ -1,9 +0,0 @@
-function Person (name,age)
-{
-    this.name = name;
-    this.age = age;
-}
-
-const person1 = new Person("dave",24);
-console.log("naem:" + ' ' + person1.name);
-console.log("age:" + ' ' + person1.age);
